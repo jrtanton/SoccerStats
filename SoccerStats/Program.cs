c960220abcfa65038d6978a150776d40e66a8672
Jarrod Tanton <jarrod.tanton@reappdev.com>
2016-10-20 15:25:55 -0400

    Add ReadSoccerResults method and Soccor Result Class.

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -12,15 +12,46 @@ namespace SoccerStats
         static void Main(string[] args)
         {
 
+
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadSoccerResults(fileName);  
 
-            var files = directory.GetFiles("*.txt");
+        }
 
-            foreach(var file in files)
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
             {
-                Console.WriteLine(file.Name);
+                return reader.ReadToEnd();
             }
+        }
+
+        public static List<string[]> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<String[]>();
+
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+                while((line = reader.ReadLine()) != null)
+                {
+                    var gameResult = new GameResult();
+                    string[] values = line.Split(',');
+
+                    DateTime gameDate;
+                    if (DateTime.TryParse(values[0], out gameDate))
+                    {
+                        gameResult.GameDate = gameDate;
+                    };
+
+                    soccerResults.Add(values);
+                }
+            }
+
+            return soccerResults;
 
         }
     }
