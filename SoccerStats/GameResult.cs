77f05624f0a9888b55f887e34053e9715dcf978f
Jarrod Tanton <jarrod.tanton@reappdev.com>
2016-10-20 18:34:14 -0400

    Add logic to read CSV file of Soccer Stats into application.

--- SoccerStats/GameResult.cs
+++ SoccerStats/GameResult.cs
@@ -9,7 +9,26 @@ namespace SoccerStats
     public class GameResult
     {
         public DateTime GameDate { get; set; }
-        public 
-
+        public String TeamName { get; set; }
+        public HomeOrAway HomeOrAway {get; set;}
+        public int Goals { get; set; }
+        public int GoalAttempts { get; set; }
+        public int ShotsOnGoal { get; set; }
+        public int ShotsOffGoal { get; set; }
+        public double PosessionPercent { get; set; }
+        public double ConversionRate
+        {
+            get
+            {
+                return (double)Goals / GoalAttempts;
+            }
+        }
     }
+
+    public enum HomeOrAway
+    {
+        Home,
+        Away
+    }
+
 }
--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -28,9 +28,9 @@ namespace SoccerStats
             }
         }
 
-        public static List<string[]> ReadSoccerResults(string fileName)
+        public static List<GameResult> ReadSoccerResults(string fileName)
         {
-            var soccerResults = new List<String[]>();
+            var soccerResults = new List<GameResult>();
 
             using (var reader = new StreamReader(fileName))
             {
@@ -46,8 +46,38 @@ namespace SoccerStats
                     {
                         gameResult.GameDate = gameDate;
                     };
+                    gameResult.TeamName = values[1];
+                    HomeOrAway homeOrAway;
+                    if(Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
 
-                    soccerResults.Add(values);
+                    int parseInt;
+                    if (int.TryParse(values[3], out parseInt))
+                    {
+                        gameResult.Goals = parseInt;
+                    }
+                    if (int.TryParse(values[4], out parseInt))
+                    {
+                        gameResult.GoalAttempts = parseInt;
+                    }
+                    if (int.TryParse(values[5], out parseInt))
+                    {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse(values[6], out parseInt))
+                    {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+
+                    double parseDouble;
+                    if(double.TryParse(values[7],out parseDouble))
+                    {
+                        gameResult.PosessionPercent = parseDouble;
+                    }
+                    
+                    soccerResults.Add(gameResult);
                 }
             }
 
