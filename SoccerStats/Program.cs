77e94a8635d0dce0d55a23994ca076eb031f440e
Jarrod Tanton <jarrod.tanton@reappdev.com>
2016-10-20 14:20:42 -0400

    Add code to print out files in current directory.

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -11,6 +11,17 @@ namespace SoccerStats
     {
         static void Main(string[] args)
         {
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+
+            var files = directory.GetFiles("*.txt");
+
+            foreach(var file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+
         }
     }
 }
